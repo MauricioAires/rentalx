bf1fa963dde75c1c2518a3cca3f5cda03e42267a
MauricioAires <mauricioaires015@gmail.com>
2023-07-20 17:08:22 -0300

    feat(typeorm specifications) add notations

--- src/modules/cars/entities/Specification.ts
+++ src/modules/cars/entities/Specification.ts
@@ -1,9 +1,15 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn } from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 
+@Entity("specifications")
 export class Specification {
+  @PrimaryColumn()
   id?: string;
+  @Column()
   name: string;
+  @Column()
   description: string;
+  @CreateDateColumn()
   created_at: Date;
 
   constructor() {
