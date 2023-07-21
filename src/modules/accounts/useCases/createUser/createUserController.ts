ee4b323e03c67fd8ad67e41c1ea0ed8f3c9a4c67
MauricioAires <mauricioaires015@gmail.com>
2023-07-21 13:56:19 -0300

    feat(user controller) create user

---
+++ src/modules/accounts/useCases/createUser/createUserController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { CreateUserUseCase } from "./createUserUseCase";
+
+export class UserController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    try {
+      const { name, username, email, password, driver_license } = req.body;
+
+      const createUserUseCase = container.resolve(CreateUserUseCase);
+
+      await createUserUseCase.execute({
+        name,
+        username,
+        email,
+        password,
+        driver_license,
+      });
+
+      return res.status(201).send();
+    } catch (error) {
+      return res.status(300).json({
+        error: error.message,
+      });
+    }
+  }
+}
