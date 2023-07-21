1859a5e2cd62b175433f7c4875b81d544b585b1e
MauricioAires <mauricioaires015@gmail.com>
2023-07-21 13:50:46 -0300

    feat(tsyringe) container user

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,10 @@
 import { container } from "tsyringe";
 
+import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
+import { UserRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
-import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
 import { ISpecificationRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
+import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
 import { SpecificationRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
 
 container.registerSingleton<ICategoriesRepository>(
@@ -14,3 +16,5 @@ container.registerSingleton<ISpecificationRepository>(
   "SpecificationRepository",
   SpecificationRepository,
 );
+
+container.registerSingleton<IUsersRepository>("UserRepository", UserRepository);
