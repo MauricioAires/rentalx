01927cd880be604e7d5436d1bbe3093af9cccc57
MauricioAires <mauricioaires015@gmail.com>
2023-07-24 10:25:04 -0300

    feat(specifications) auth router

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -2,13 +2,16 @@ import { Router } from "express";
 
 import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/createSpecificationController";
 import { ListSpecificationController } from "../modules/cars/useCases/listSpecification/listSpecificationController";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const specificationsRouter = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 const listSpecificationController = new ListSpecificationController();
 
-specificationsRouter.post("/", createSpecificationController.handle);
 specificationsRouter.get("/", listSpecificationController.handle);
+
+specificationsRouter.use(ensureAuthenticated);
+specificationsRouter.post("/", createSpecificationController.handle);
 
 export { specificationsRouter };
