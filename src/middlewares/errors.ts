57ebe92e91326b9e78223fb86a789ecdeacd9826
MauricioAires <mauricioaires015@gmail.com>
2023-07-24 11:57:19 -0300

    feat(middleware) handle app erros

---
+++ src/middlewares/errors.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "../errors/AppError";
+
+export async function handleAppErrors(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
+
+  // NOTE: Lidar com error não identificado
+  return res.status(500).json({
+    status: "error",
+    message: `Internal server error - ${err.message}`,
+  });
+}
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
 import "./database";
 import "./shared/container";
 
+import { handleAppErrors } from "./middlewares/errors";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
@@ -14,5 +16,7 @@ app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
+
+app.use(handleAppErrors);
 
 app.listen(3333, () => console.log("server listening on port 3333"));
