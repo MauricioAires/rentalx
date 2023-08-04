b2985ccfcf5972ba2d5f77d180afe113f1562070
MauricioAires <mauricioaires015@gmail.com>
2023-08-03 23:22:55 -0300

    feat(repositories) create categories in memory

---
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,36 @@
+import { Category } from "../../entities/Category";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "../ICategoriesRepository";
+
+export class CategoriesRepositoryInMemory implements ICategoriesRepository {
+  categories: Category[] = [];
+
+  async findByName(name: string): Promise<Category> {
+    const category = this.categories.find((c) => c.name === name);
+
+    return category;
+  }
+
+  async list(): Promise<Category[]> {
+    return this.categories;
+  }
+
+  async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+    let category = new Category();
+
+    category = {
+      ...category,
+      name,
+      description,
+    };
+
+    Object.assign(category, {
+      name,
+      description,
+    });
+
+    this.categories.push(category);
+  }
+}
