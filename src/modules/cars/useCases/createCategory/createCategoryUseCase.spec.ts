7cba2c876820b41906470d97370c77ac329d840e
MauricioAires <mauricioaires015@gmail.com>
2023-08-03 23:23:31 -0300

    test(categories) teste create category

---
+++ src/modules/cars/useCases/createCategory/createCategoryUseCase.spec.ts
@@ -0,0 +1,43 @@
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { AppError } from "../../../../errors/AppError";
+
+describe("Create Category", () => {
+  let createCategoryUseCase: CreateCategoryUseCase;
+  let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(
+      categoriesRepositoryInMemory,
+    );
+  });
+
+  it("should be able to create a new category", async () => {
+    const category = {
+      description: "Description Test",
+      name: "Category Teste",
+    };
+
+    await createCategoryUseCase.execute(category);
+
+    const categoryCreated = await categoriesRepositoryInMemory.findByName(
+      category.name,
+    );
+
+    expect(categoryCreated).toHaveProperty("id");
+  });
+
+  it("should not be able to create a new category with name exists", async () => {
+    expect(async () => {
+      const category = {
+        description: "Description Test",
+        name: "Category Teste",
+      };
+
+      await createCategoryUseCase.execute(category);
+
+      await createCategoryUseCase.execute(category);
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
