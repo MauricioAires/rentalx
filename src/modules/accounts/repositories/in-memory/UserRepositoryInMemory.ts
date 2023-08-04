e5823d5c2a914ad8e2a18e1d7dedbe4d7b055247
MauricioAires <mauricioaires015@gmail.com>
2023-08-03 23:33:04 -0300

    feat(repositories) user repository in memory

---
+++ src/modules/accounts/repositories/in-memory/UserRepositoryInMemory.ts
@@ -0,0 +1,34 @@
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+
+export class UserRepositoryInMemory implements IUsersRepository {
+  users: User[] = [];
+
+  async create({
+    driver_license,
+    email,
+    name,
+    password,
+  }: ICreateUserDTO): Promise<void> {
+    let user = new User();
+
+    user = {
+      ...user,
+      driver_license,
+      email,
+      name,
+      password,
+    };
+
+    this.users.push(user);
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return this.users.find((user) => user.email === email);
+  }
+
+  async findById(id: string): Promise<User> {
+    return this.users.find((user) => user.id === id);
+  }
+}
