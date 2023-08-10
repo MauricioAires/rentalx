676e8483f76f5e3943ebc11308e95b69b4aecee8
MauricioAires <mauricioaires015@gmail.com>
2023-08-10 18:19:06 -0300

    feat(middleware)  ensure admin

---
+++ src/shared/infra/http/middleware/ensureAdmin.ts
@@ -0,0 +1,20 @@
+import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import { AppError } from "@shared/errors/AppError";
+import { NextFunction, Request, Response } from "express";
+
+export async function ensureAdmin(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const { id } = req.user;
+
+  const usersRepository = new UsersRepository();
+  const user = await usersRepository.findById(id);
+
+  if (!user.is_admin) {
+    throw new AppError("User ins'n admin");
+  }
+
+  return next();
+}
