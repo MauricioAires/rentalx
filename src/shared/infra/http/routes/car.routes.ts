1694a1e674d33cbc1f84e7f36233729e862d598a
MauricioAires <mauricioaires015@gmail.com>
2023-08-16 07:40:25 -0300

    feat(car routes) create car specification

--- src/shared/infra/http/routes/car.routes.ts
+++ src/shared/infra/http/routes/car.routes.ts
@@ -3,11 +3,13 @@ import { Router } from "express";
 import { ensureAdmin } from "@shared/infra/http/middleware/ensureAdmin";
 import { ensureAuthenticated } from "@shared/infra/http/middleware/ensureAuthenticated";
 import { ListAvailableCarController } from "@modules/cars/useCases/listAvailableCar/listAvailableCarController";
+import { CreateCarSpecificationsController } from "@modules/cars/useCases/createCarSpecification/createCarSpecificationsController";
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 const listAvailableCar = new ListAvailableCarController();
+const crateCarSpecifications = new CreateCarSpecificationsController();
 
 carsRoutes.post(
   "/",
@@ -17,5 +19,7 @@ carsRoutes.post(
 );
 
 carsRoutes.get("/available", listAvailableCar.handle);
+
+carsRoutes.get("/specifications/:id", crateCarSpecifications.handle);
 
 export { carsRoutes };
