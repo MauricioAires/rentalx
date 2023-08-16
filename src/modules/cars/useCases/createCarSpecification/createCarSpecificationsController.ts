cd2cd3a636d49abe0fda436f109e816608a2502c
MauricioAires <mauricioaires015@gmail.com>
2023-08-16 07:34:55 -0300

    feat(car specifications) create controller

---
+++ src/modules/cars/useCases/createCarSpecification/createCarSpecificationsController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { CreateCarSpecificationUseCase } from "./createCarSpecificationUseCase";
+
+export class CreateSpecificationsController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const { id: car_id } = req.params;
+    const { specifications_id } = req.body;
+
+    const createCarSpecifications = container.resolve(
+      CreateCarSpecificationUseCase,
+    );
+
+    const carSpecifications = await createCarSpecifications.execute({
+      car_id,
+      specifications_id,
+    });
+
+    return res.json(carSpecifications);
+  }
+}
