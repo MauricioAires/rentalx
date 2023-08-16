597a91bfe20fb845cb80d023dae53ab50ec2b384
MauricioAires <mauricioaires015@gmail.com>
2023-08-16 07:40:09 -0300

    feat(create car specification use case) turn injectable

--- src/modules/cars/useCases/createCarSpecification/createCarSpecificationUseCase.ts
+++ src/modules/cars/useCases/createCarSpecification/createCarSpecificationUseCase.ts
@@ -2,15 +2,19 @@ import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { ISpecificationRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 import { AppError } from "@shared/errors/AppError";
+import { inject, injectable } from "tsyringe";
 
 interface IRequest {
   car_id: string;
   specifications_id: string[];
 }
 
+@injectable()
 export class CreateCarSpecificationUseCase {
   constructor(
+    @inject("CarsRepository")
     private carsRepository: ICarsRepository,
+    @inject("SpecificationsRepository")
     private specificationsRepository: ISpecificationRepository,
   ) {}
   async execute({ car_id, specifications_id }: IRequest): Promise<Car> {
