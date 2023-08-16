1ec2d3b35a521d84573c7a209e6adbc5f50c41a9
MauricioAires <mauricioaires015@gmail.com>
2023-08-16 07:39:35 -0300

    fix(create car specification) fix class name

--- src/modules/cars/useCases/createCarSpecification/createCarSpecificationsController.ts
+++ src/modules/cars/useCases/createCarSpecification/createCarSpecificationsController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { container } from "tsyringe";
 import { CreateCarSpecificationUseCase } from "./createCarSpecificationUseCase";
 
-export class CreateSpecificationsController {
+export class CreateCarSpecificationsController {
   async handle(req: Request, res: Response): Promise<Response> {
     const { id: car_id } = req.params;
     const { specifications_id } = req.body;
