469ccab5d3778007475e7709085b707fb7d91115
MauricioAires <mauricioaires015@gmail.com>
2023-08-25 11:36:01 -0300

    fix(migration) set column user id is primary

--- src/shared/infra/typeorm/migrations/1689950646883-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1689950646883-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1689950646883 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
