2670190459454b9ffdf8dc0c6aaaae51fa9ad209
MauricioAires <mauricioaires015@gmail.com>
2023-08-25 09:54:03 -0300

    feat(routes) create router upload car images

--- src/shared/infra/http/routes/car.routes.ts
+++ src/shared/infra/http/routes/car.routes.ts
@@ -1,15 +1,25 @@
-import { CreateCarController } from "@modules/cars/useCases/createCar/createCarController";
 import { Router } from "express";
+
 import { ensureAdmin } from "@shared/infra/http/middleware/ensureAdmin";
 import { ensureAuthenticated } from "@shared/infra/http/middleware/ensureAuthenticated";
+
+import { CreateCarController } from "@modules/cars/useCases/createCar/createCarController";
 import { ListAvailableCarController } from "@modules/cars/useCases/listAvailableCar/listAvailableCarController";
 import { CreateCarSpecificationsController } from "@modules/cars/useCases/createCarSpecification/createCarSpecificationsController";
+import { UploadCarImagesController } from "@modules/cars/useCases/uploadCarImages/uploadCarImagesController";
+
+import upload from "@config/upload";
+import multer from "multer";
 
 const carsRoutes = Router();
 
+const uploadCarImages = multer(upload.execute("./tmp/cars"));
+
 const createCarController = new CreateCarController();
-const listAvailableCar = new ListAvailableCarController();
-const crateCarSpecifications = new CreateCarSpecificationsController();
+const listAvailableCarController = new ListAvailableCarController();
+const crateCarSpecificationsController =
+  new CreateCarSpecificationsController();
+const uploadCarImagesController = new UploadCarImagesController();
 
 carsRoutes.post(
   "/",
@@ -18,13 +28,21 @@ carsRoutes.post(
   createCarController.handle,
 );
 
-carsRoutes.get("/available", listAvailableCar.handle);
+carsRoutes.get("/available", listAvailableCarController.handle);
 
 carsRoutes.post(
   "/specifications/:id",
   ensureAuthenticated,
   ensureAdmin,
-  crateCarSpecifications.handle,
+  crateCarSpecificationsController.handle,
+);
+
+carsRoutes.post(
+  "/images/:id",
+  ensureAuthenticated,
+  ensureAdmin,
+  uploadCarImages.array("images"),
+  uploadCarImagesController.handle,
 );
 
 export { carsRoutes };
