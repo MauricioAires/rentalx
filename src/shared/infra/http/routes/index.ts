b785d6cf9019b8f1bf12f1375a3764e18b041e21
MauricioAires <mauricioaires015@gmail.com>
2023-08-28 14:17:28 -0300

    feat(routes) rental

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -4,6 +4,7 @@ import { specificationsRouter } from "./specifications.routes";
 import { usersRoutes } from "./users.routes";
 import { authenticateRoutes } from "./authenticate.routes";
 import { carsRoutes } from "./car.routes";
+import { rentalRoutes } from "./rental.routes";
 
 const router = Router();
 
@@ -11,6 +12,7 @@ router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationsRouter);
 router.use("/users", usersRoutes);
 router.use("/cars", carsRoutes);
+router.use("/rentals", rentalRoutes);
 router.use(authenticateRoutes);
 
 export { router };
