4ab0592ee37a289f638302298f4286268df3bece
MauricioAires <mauricioaires015@gmail.com>
2023-08-28 11:21:49 -0300

    feat(repositories) create interface and in memory

---
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -0,0 +1,8 @@
+import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
+import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDTO";
+
+export interface IRentalsRepository {
+  findOpenRentalByCarId(car_id: string): Promise<Rental>;
+  findOpenRentalByUserId(user_id: string): Promise<Rental>;
+  create(data: ICreateRentalDTO): Promise<Rental>;
+}
