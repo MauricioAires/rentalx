5b17b94661e90f0c0a4e351654bd7a756204ca32
MauricioAires <mauricioaires015@gmail.com>
2023-08-29 11:44:08 -0300

    feat(connection) set environment database and host

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -3,9 +3,12 @@ import { Connection, createConnection, getConnectionOptions } from "typeorm";
 export default async (host = "database"): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
-  return createConnection(
-    Object.assign(defaultOptions, {
-      host,
-    }),
-  );
+  const isTestEnvironment = process.env.NODE_ENV === "test";
+
+  Object.assign(defaultOptions, {
+    host: isTestEnvironment ? "localhost" : host,
+    database: isTestEnvironment ? "rentx_test" : defaultOptions.database,
+  });
+
+  return createConnection(defaultOptions);
 };
