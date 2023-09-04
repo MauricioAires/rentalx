f4727dfbae274b3e0bda362c664093ac8b164f97
MauricioAires <mauricioaires015@gmail.com>
2023-09-04 17:24:47 -0300

    feat(routes) create route devolution rental

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -2,11 +2,18 @@ import { Router } from "express";
 
 import { ensureAuthenticated } from "@shared/infra/http/middleware/ensureAuthenticated";
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/createRentalController";
+import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/devolutionRentalController";
 
 const rentalRoutes = Router();
 
 const createRentalController = new CreateRentalController();
+const devolutionRentalController = new DevolutionRentalController();
 
 rentalRoutes.post("/", ensureAuthenticated, createRentalController.handle);
+rentalRoutes.post(
+  "/devolution/:id",
+  ensureAuthenticated,
+  devolutionRentalController.handle,
+);
 
 export { rentalRoutes };
