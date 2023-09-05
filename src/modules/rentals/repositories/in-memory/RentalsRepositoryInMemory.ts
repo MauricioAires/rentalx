f95a29b180168daec2a06a9bebec67d98ccfb93e
MauricioAires <mauricioaires015@gmail.com>
2023-09-05 17:20:26 -0300

    feat(rentals repository) add methods required

--- src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
+++ src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
@@ -35,4 +35,12 @@ export class RentalsRepositoryInMemory implements IRentalsRepository {
       (rental) => rental.user_id === user_id && !rental.end_date,
     );
   }
+
+  async findById(id: string): Promise<Rental> {
+    return this.rentals.find((rental) => rental.id === id);
+  }
+
+  async findByUserId(user_id: string): Promise<Rental[]> {
+    return this.rentals.filter((rental) => rental.user_id === user_id);
+  }
 }
