21a7ab3e3eb0ae6695ed9d1ac44065dd37bb2dc3
MauricioAires <mauricioaires015@gmail.com>
2023-09-06 09:23:17 -0300

    fix(car repository memory) create car

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -14,7 +14,6 @@ export class CarsRepositoryInMemory implements ICarsRepository {
     fine_amount,
     brand,
     category_id,
-    id,
   }: ICreateCarDTO): Promise<Car> {
     const car = new Car();
 
@@ -26,7 +25,6 @@ export class CarsRepositoryInMemory implements ICarsRepository {
       fine_amount,
       brand,
       category_id,
-      id,
     });
 
     this.cars.push(car);
