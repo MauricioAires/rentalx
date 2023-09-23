7a7903c2ca1485e2ebb78e7151b338ba4349fa46
MauricioAires <mauricioaires015@gmail.com>
2023-09-23 16:44:44 -0300

    feat(routes) add reset password

--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
 import { SendForgotPasswordMailController } from "@modules/accounts/useCases/sendForgotPasswordMail/sendForgotPasswordMailController";
+import { ResetPasswordUserController } from "@modules/accounts/useCases/resetPasswordUser/resetPasswordUserController";
 
 const passwordRoutes = Router();
 
 const sendForgotPasswordMailController = new SendForgotPasswordMailController();
+const resetPasswordUserController = new ResetPasswordUserController();
 
 passwordRoutes.post("/forgot", sendForgotPasswordMailController.handle);
+passwordRoutes.post("/reset", resetPasswordUserController.handle);
 
 export { passwordRoutes };
