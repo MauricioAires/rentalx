c8eed4f3c1110222db6d36b92041632bebc5b4b7
MauricioAires <mauricioaires015@gmail.com>
2023-09-22 23:30:00 -0300

    feat(date provider) add days

--- src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -29,4 +29,8 @@ export class DayjsDateProvider implements IDateProvider {
 
     return dayjs(start_date_utc).diff(end_date_utc, "days");
   }
+
+  addDays(days: number): Date {
+    return dayjs().add(days, "days").toDate();
+  }
 }
