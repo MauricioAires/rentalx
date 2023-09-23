4fb2c293cfa0fa2f1d2c590190e780248d1ce4f4
MauricioAires <mauricioaires015@gmail.com>
2023-09-23 15:18:03 -0300

    feat(use case) forgot password

---
+++ src/modules/accounts/useCases/sendForgotPasswordMail/sendForgotPasswordMailUseCase.ts
@@ -0,0 +1,46 @@
+import { inject, injectable } from "tsyringe";
+import { v4 as uuidV4 } from "uuid";
+
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+import { AppError } from "@shared/errors/AppError";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+import { IMailProvider } from "@shared/container/providers/MailProvider/IMailProvider";
+
+@injectable()
+export class SendForgotPasswordMailUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository,
+    @inject("UsersTokensRepository")
+    private usersTokensRepository: IUsersTokensRepository,
+    @inject("DayjsDateProvider")
+    private dateProvider: IDateProvider,
+    @inject("EtherealMailProvider")
+    private mailProvider: IMailProvider,
+  ) {}
+
+  async execute(email: string) {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError("User not found!", 404);
+    }
+
+    const token = uuidV4();
+
+    await this.usersTokensRepository.create({
+      refresh_token: token,
+      user_id: user.id,
+      expires_date: this.dateProvider.addHours(3),
+    });
+
+    await this.mailProvider.sendEmail(
+      email,
+      "Recuperação de senha ",
+      `
+        O link para o reset é ${token}
+      `,
+    );
+  }
+}
