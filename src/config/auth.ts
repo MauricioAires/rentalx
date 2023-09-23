9f4d6bd55eb5ef230e6a8f4eeac1830af7bf9a5c
MauricioAires <mauricioaires015@gmail.com>
2023-09-22 23:28:47 -0300

    feta(auth) variables

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "22d93c2a430a360d00ea2eaf25635381",
+  expires_token: "15m",
+  secrete_refresh_token: "fd254f723587bb77667619cb67142433",
+  expires_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
