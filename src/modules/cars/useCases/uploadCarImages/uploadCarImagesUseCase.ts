d49a5c1a030638cb96b874f9588b484844ab4222
MauricioAires <mauricioaires015@gmail.com>
2023-09-29 18:22:55 -0300

    refactor(car) upload image provider

--- src/modules/cars/useCases/uploadCarImages/uploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImages/uploadCarImagesUseCase.ts
@@ -1,4 +1,5 @@
 import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider";
 import { inject, injectable } from "tsyringe";
 
 interface IRequest {
@@ -11,11 +12,14 @@ export class UploadCarImagesUseCase {
   constructor(
     @inject("CarsImagesRepository")
     private carsImagesRepository: ICarsImagesRepository,
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider,
   ) {}
 
   async execute({ car_id, images_names }: IRequest): Promise<void> {
     images_names.forEach((image_name) => {
       this.carsImagesRepository.create(car_id, image_name);
+      this.storageProvider.save(image_name, "cars");
     });
   }
 }
