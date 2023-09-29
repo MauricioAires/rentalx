728f4956be6b4cf48f37f206687e793aae5dfe08
MauricioAires <mauricioaires015@gmail.com>
2023-09-29 18:14:42 -0300

    feat(app) redirect routes

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express from "express";
 import "express-async-errors";
 
@@ -9,6 +10,7 @@ import "@shared/container";
 import { handleAppErrors } from "./middleware/errors";
 import { router } from "@shared/infra/http/routes";
 import swaggerFile from "../../../swagger.json";
+import upload from "@config/upload";
 
 createConnection();
 
@@ -17,6 +19,10 @@ const app = express();
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+/** Realizar redirecionamento de requisição */
+app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(router);
 
