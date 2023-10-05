6e6280818c6c80c83898a37f9fe0b81eac317117
MauricioAires <mauricioaires015@gmail.com>
2023-10-05 00:07:36 -0300

    feat(app) config Sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -14,11 +14,35 @@ import swaggerFile from "../../../swagger.json";
 import upload from "@config/upload";
 import cors from "cors";
 
+import * as Sentry from "@sentry/node";
+import { ProfilingIntegration } from "@sentry/profiling-node";
+
 createConnection();
 
 const app = express();
 
 app.use(rateLimitMiddleware);
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Sentry.Integrations.Express({ app }),
+    new ProfilingIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+});
+
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
+
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use(express.json());
 
@@ -34,6 +58,18 @@ app.use(
   }),
 );
 app.use(router);
+
+app.use(
+  Sentry.Handlers.errorHandler({
+    shouldHandleError(err) {
+      if (err.statusCode === 404 || err.statusCode === 500) {
+        return true;
+      }
+
+      return false;
+    },
+  }),
+);
 
 app.use(handleAppErrors);
 
